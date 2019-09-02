6de05a2b233d98a705367ea8d8c84ce16517ef08
Fernando Mascarenhas <fernando-h-l-mascarenhas@gmail.com>
2019-09-02 18:39:39 -0300

    Very Simple API done using the .substring (own solution) to get the ID Code may chance considereing lesson 16 - Parsing Variables from URLs

--- index.js
+++ index.js
@@ -39,23 +39,69 @@ const url = require('url');
 //////////////////////////////////////
 // SERVER
 
-// In this case we use the Sync version because the function will only execute once when the code starts, blocking the execution for a few ms
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic){
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    } 
+
+    return output;
+};
+
+
+// In these cases we use the Sync version because the function will only execute once when the code starts, blocking the execution for a few ms
+const tempOverview = fs.readFileSync (`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync (`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync (`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync (`${__dirname}/dev-data/data.json`, 'utf-8')
-const dataObj = JSON.parse(data);   
+const dataObj = JSON.parse(data);
 
 const server = http.createServer ((req, res) => {
     const pathName = req.url;
 
+
+    // Overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.end ('This is the OVERVIEW');
-    } else if (pathName === '/product'){
-        res.end ('This is the PRODUCT')
+        res.writeHead (200, {'Content-type': 'text/html'})        
+        
+        // Use map to create an array with all the cards
+        const cardsHtml = dataObj.map (el => replaceTemplate (tempCard, el)) 
+        
+        // Replace the {%PRODUCT_CARDS%} with a string resulting of all elements of the cardsHtml array
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml.join());
+
+        res.end (output);
+
+    // Product Page
+    } else if (pathName.substring(0, pathName.indexOf("?")) === '/product'){
+        res.writeHead (200, {'Content-type': 'text/html'})    
+
+        // Geting verything after the =
+        const id = pathName.substring(pathName.indexOf("=")+1)
+
+        // Replacing the tenplate with the info from the dataObj
+        const output = replaceTemplate (tempProduct, dataObj[id]);
+        
+        res.end (output);
+
+
+    // API        
     } else if (pathName === '/api'){
         // We need to inform the brownser that we are returning json ... We use the code 200 that represent the status sucess
         res.writeHead (200, {'Content-type':'application/json'});
         // res.end need to send back an string, so we can pass the data
         res.end (data);
         
+    // Not found
     }else {
         res.writeHead(404, {
             // The browser will now expect an html and need to be set before we send the response
